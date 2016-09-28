a0e3edb26883d435126b0934e68fcaf466676120
Yulia Gaponenko <barthy@yandex.ru>
2016-09-28 15:03:25 +0200

    ppc64le update

--- scripts/binary-builder-integration.sh
+++ scripts/binary-builder-integration.sh
@@ -12,8 +12,8 @@ pushd binary-builder
 
   if [ ${RUN_ORACLE_PHP_TESTS-false} = "true" ]; then
     apt-get update && apt-get -y install awscli
-    bundle exec rspec spec/integration/${SPEC_TO_RUN}_spec.rb
+    BINARY_BUILDER_PLATFORM=${BINARY_BUILDER_PLATFORM} bundle exec rspec spec/integration/${SPEC_TO_RUN}_spec.rb
   else
-    bundle exec rspec spec/integration/${SPEC_TO_RUN}_spec.rb --tag ~run_oracle_php_tests
+    BINARY_BUILDER_PLATFORM=${BINARY_BUILDER_PLATFORM} bundle exec rspec spec/integration/${SPEC_TO_RUN}_spec.rb --tag ~run_oracle_php_tests
   fi
 popd
